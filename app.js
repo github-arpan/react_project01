5afed4c5b51f5164c8243437305e2eb8bf11cd02
subroto18 <subrotochakraborty18@gmail.com>
2024-05-31 20:39:15 +0530

    This is an simple react project

---
+++ app.js
@@ -0,0 +1,16 @@
+const parent = React.createElement("div", {id: "parent"},[
+    React.createElement("div", {id : "child"}, 
+[React.createElement("h1", {}, "hello world"),
+React.createElement("h2", {}, "hello world 2")]
+)
+],[
+    React.createElement("div", {id : "child2"}, 
+[React.createElement("h1", {}, "hello world"),
+React.createElement("h2", {}, "hello world 2")]
+)
+]
+
+)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent) 
