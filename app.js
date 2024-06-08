cf2eceeb70a8f2ea6596080d2f5a35d5d61c4345
github-arpan <develop.arpan@gmail.com>
2024-06-09 00:10:02 +0530

    building food delivery app

--- app.js
+++ app.js
@@ -2,25 +2,57 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const HeadingComponent = () => (
-    <>
-    <h1>Hello form heading componet</h1>
-    <Component2/>
-    </>
-   
-)
-   
+const Header = ()=>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo"
+                src="https://png.pngtree.com/png-clipart/20220604/original/pngtree-restaurant-logo-png-image_7932128.png"/>
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+const Body = () =>{
+return (
+    <div className="body">
+        <div className="search-bar">
+            <input placeholder="Find What u like"></input>
+            <button>Search</button>
+        </div>
+        <div className="res-container"></div>
+    </div>
     
+)
+}
 
 
-const Component2 = () => <h1>hello form HeadingComponent2</h1>
 
 
+const AppLayout =()=>{
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
 
-const jsxheading = <h1 id="heading" className="header" tabIndex={1}>hello JSX</h1>
+        </div>
+    )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<HeadingComponent/>)
+root.render(<AppLayout/>)
 
 
 
