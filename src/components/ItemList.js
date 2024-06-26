3b2a2d4d3a26cd7fa0abc9ea3ccedaa429e85b7a
github-arpan <develop.arpan@gmail.com>
2024-06-26 17:23:27 +0530

    build accordion for menu list

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,29 +6,34 @@ const ItemList = ({ items }) => {
       {items.map((item) => (
         <div
           key="item?.card?.info?.id"
-          className=" py-2 my-2 border-b border-gray-300 flex justify-between"
+          className=" py-2 my-2  border-b border-gray-300 flex justify-between"
         >
-          <div className="lg:w-9/12 flex flex-col justify-between">
+          <div className="w-9/12 flex flex-col justify-between ">
             <div className="font-bold">
               <span>{item?.card?.info?.name}</span>
-              <span className="block">
+              <span className="block my-1">
                 ₹{" "}
                 {item?.card?.info?.price / 100 ||
                   item?.card?.info?.defaultPrice / 100}
               </span>
             </div>
             <div>
-              <p className="my-4 py-2  text-xs ">
+              <p className="my-4 py-2 mb-6  text-xs ">
                 {item?.card?.info?.description}
               </p>
             </div>
           </div>
 
-          <div className="w-[156px] h-36  ">
+          <div className="w-[156px] h-36 p-2 m-2">
             <img
               src={CDN_URL + item?.card?.info?.imageId}
               className=" rounded-md w-full h-full "
             />
+            <div className="relative bottom-8 left-7 py-2   ">
+              <button className="py-2 px-6  text-green-600 border border-gray-300 bg-white shadow-lg font-bold rounded-md ">
+                ADD
+              </button>
+            </div>
           </div>
         </div>
       ))}
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,10 +1,20 @@
 import { FaChevronDown } from "react-icons/fa";
 import ItemList from "./ItemList";
+import { useState } from "react";
+
 const RestaurantCategory = ({ data }) => {
+  const [showItems, setShowItem] = useState(false);
+
+  const handleClick = () => {
+    setShowItem(!showItems);
+  };
   console.log(data);
   return (
-    <div className="bg-gray-100 shadow-lg p-4 my-4 cursor-pointer">
-      <div className="flex justify-between items-center">
+    <div className="bg-gray-100 shadow-lg p-4 my-4 ">
+      <div
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
         <span className="font-bold">
           {data.title} ({data.itemCards.length})
         </span>
@@ -12,7 +22,7 @@ const RestaurantCategory = ({ data }) => {
           <FaChevronDown />
         </span>
       </div>
-      <ItemList items={data.itemCards} />
+      {showItems && <ItemList items={data.itemCards} />}
     </div>
   );
 };
