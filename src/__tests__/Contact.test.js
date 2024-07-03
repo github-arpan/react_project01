2e8d0bf1840c552c55bdb32266c0c32255f22315
github-arpan <develop.arpan@gmail.com>
2024-07-03 22:46:45 +0530

    testing

--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -2,15 +2,26 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
 import "@testing-library/jest-dom";
 
-test(" should load contact us component", () => {
-  render(<Contact />);
+describe(" Contact us page test case", () => {
+  it("should load contact us component", () => {
+    render(<Contact />);
 
-  const heading = screen.getByRole("heading");
+    const heading = screen.getByRole("heading");
 
-  expect(heading).toBeInTheDocument();
-});
-test("Button should be there inside the component", () => {
-  render(<Contact />);
-  const Button = screen.getByText("Submit");
-  expect(Button).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("Button should be there inside the component", () => {
+    render(<Contact />);
+    const Button = screen.getByText("Submit");
+    expect(Button).toBeInTheDocument();
+  });
+
+  it("input should be there inside the component", () => {
+    render(<Contact />);
+
+    const inputboxs = screen.getAllByRole("textbox");
+
+    expect(inputboxs.length).toBe(2);
+  });
 });
